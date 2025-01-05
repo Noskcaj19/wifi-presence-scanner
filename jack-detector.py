e394efdaca9a42c63ead6ee863988c272cfd048d
Noskcaj <kcajdev@gmail.com>
2025-01-05 12:03:41 -0500

    webapp tweaks

--- jack-detector.py
+++ jack-detector.py
@@ -5,14 +5,14 @@ from main import get_users
 
 app = FastAPI()
 
-tracked_macs = set(map(lambda l: l.split(" ")[0], open("macs.txt").readlines()))
+tracked_macs = set(map(lambda l: l.split()[0], open("macs.txt").readlines()))
 print(tracked_macs)
 
 
 @app.get("/")
 def read_root():
-    online_users = get_users()
+    users = get_users()
+    online_users = {m: u for m, u in users.items() if u["Status"] == "on"}
     if not set(online_users.keys()).isdisjoint(tracked_macs):
         return FileResponse("html/present.html")
-    else:
-        return FileResponse("html/absent.html")
+    return FileResponse("html/absent.html")
